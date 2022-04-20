c1536863533966f295f1fc09fc7ed425735f57fb
hugo-fasth-nti-johanneberg <hugo.fasth@elev.ga.ntig.se>
2022-04-20 11:41:35 +0200

    Arbeta med loginnet idag nästan funkar nu

--- app.rb
+++ app.rb
@@ -23,9 +23,9 @@ get '/logout' do
 end
 
 post ('/users/new') do
-    username = params["username"]
-    password = params["password"]
-    password_confirmation = params["password_confirmation"]
+    username = params[:username]
+    password = params[:password]
+    password_confirmation = params[:password_confirmation]
 
     db = SQLite3::Database.new("db/dbSlutprojekt.db")
     db.results_as_hash = true
@@ -36,8 +36,8 @@ post ('/users/new') do
         if password == password_confirmation
             password_digest = BCrypt::Password.create(password)
             p password_digest
-            db.execute("INSERT INTO users(username, password_digest) VALUES (?,?)", [username, password_digest])
-            redirect('/register_confirmation')
+            db.execute("INSERT INTO users (username, password_digest) VALUES (?,?)", username, password_digest)
+            redirect('/')
         else
             #kanske popup om orkar
             redirect('/error')
@@ -49,7 +49,31 @@ end
 
 
 get '/showlogin' do
-    "Shuno mannenguz"
+
+    username = params[:username]
+    password = params[:password]
+
+    db = SQLite3::Database.new("db/dbSlutprojekt.db")
+    db.results_as_hash = true
+
+    result = db.execute("SELECT userid, password_digest FROM users WHERE username=?", username)
+
+    if result.empty?
+        # redirect('/error')
+    end                 #FIXA NÄSTA GÅNG VETTE FAN
+    
+    p result
+    if result.length != 0
+        user_id = result.first["userid"]    
+    end    
+    password_digest = result.first["passowrd_digest"]
+    if BCryt::Password.new(password_digest) == password
+        session[:user_id] = user_id
+        redirect('/lists')
+    else
+        set_error("Ivalid Credentials")
+        redirect('/error')
+    end
     slim(:showlogin)
 end
 
@@ -75,24 +99,3 @@ end
 
 ##DOKUMENTERA NÄSTA GÅNGq
 
-# username = params["username"]
-#     password = params["password"]
-
-#     db = SQLite3::Database.new("db/dbSlutprojekt.db")
-#     db.results_as_hash = true
-
-#     result = db.execute("SELECT userid, password_digest FROM users WHERE username=?", [username])
-
-#     if result.empty?
-#         redirect('/error')
-#     end
-
-#     user_id = result.first["id"]
-#     password_digest = result.first["passowrd_digest"]
-#     if BCryt::Password.new(password_digest) == password
-#         session[:user_id] = user_id
-#         redirect('/lists')
-#     else
-#         set_error("Ivalid Credentials")
-#         redirect('/error')
-#     end
